337f8c25921b2faacca3f5ea0d604fe4bb13423c
Matthew Stoecklein <matildajsnail@gmail.com>
2024-03-05 18:00:16 -0700

    Add general HTML and CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'snail.id';
+  linkedinLink = 'https://www.linkedin.com/in/matilda-snail-422823287';
+  githubLink = 'https://github.com/JulySnail';
 }
