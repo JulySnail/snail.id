55f060bc5cbf34c026a6ecdf076112f5567f303a
“matildajuly” <matilajsnail@gmail.com>
2024-03-24 13:09:00 -0600

    Add about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, AboutComponent, AboutComponent],
 })
 export class AppComponent {
   title = 'snail.id';
   linkedinLink = 'https://www.linkedin.com/in/matilda-snail-422823287';
   githubLink = 'https://github.com/JulySnail';
+  showAboutComponent: boolean = false;
+  toggleAboutComponent() {
+    this.showAboutComponent = !this.showAboutComponent;
+  }
 }
